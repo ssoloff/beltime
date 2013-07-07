44d199d86395249dee7892a072ea714e842bf995
ssoloff <s_soloff@yahoo.com>
2013-07-06 22:36:17 -0400

    Added package information file.

---
+++ com.belcan.beltime/src/com/belcan/beltime/package-info.java
@@ -0,0 +1,21 @@
+/*
+ * package-info.java
+ *
+ * Copyright 2013 Beltime contributors and others.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Public License v1.0, which
+ * accompanies this distribution and is available at
+ * <http://www.eclipse.org/legal/epl-v10.html>.
+ *
+ * Created on Jul 6, 2013 at 10:11:27 PM.
+ */
+
+/**
+ * Defines the Beltime application.
+ */
+@NonNullByDefault
+package com.belcan.beltime;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
