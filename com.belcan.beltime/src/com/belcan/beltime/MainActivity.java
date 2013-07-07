dc63c80c2529a3265a3ea4c7fc8f80b11dd79bc3
ssoloff <s_soloff@yahoo.com>
2013-07-06 22:35:50 -0400

    Added Java compiler error/warning preferences. Enabled null analysis.

--- com.belcan.beltime/src/com/belcan/beltime/MainActivity.java
+++ com.belcan.beltime/src/com/belcan/beltime/MainActivity.java
@@ -1,5 +1,6 @@
 package com.belcan.beltime;
 
+import org.eclipse.jdt.annotation.Nullable;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -7,13 +8,13 @@ import android.view.Menu;
 public class MainActivity extends Activity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(@Nullable Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
