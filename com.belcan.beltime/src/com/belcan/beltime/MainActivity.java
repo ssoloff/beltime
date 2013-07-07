15560addb145a58f0c91de6e936d9b0dbaff585e
ssoloff <s_soloff@yahoo.com>
2013-07-06 22:42:21 -0400

    Reformatted code.

--- com.belcan.beltime/src/com/belcan/beltime/MainActivity.java
+++ com.belcan.beltime/src/com/belcan/beltime/MainActivity.java
@@ -1,3 +1,17 @@
+/*
+ * MainActivity.java
+ *
+ * Copyright 2013 Beltime contributors and others.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Public License v1.0, which
+ * accompanies this distribution and is available at
+ * <http://www.eclipse.org/legal/epl-v10.html>.
+ *
+ * Created on Jul 5, 2013 at 10:29:09 PM.
+ */
+
 package com.belcan.beltime;
 
 import android.app.Activity;
@@ -5,19 +19,50 @@ import android.os.Bundle;
 import android.view.Menu;
 import org.eclipse.jdt.annotation.Nullable;
 
-public class MainActivity extends Activity {
+/**
+ * The main activity.
+ */
+public final class MainActivity
+    extends Activity
+{
+    // ======================================================================
+    // Constructors
+    // ======================================================================
 
-	@Override
-	protected void onCreate(@Nullable Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    /**
+     * Initializes a new instance of the {@code MainActivity} class.
+     */
+    public MainActivity()
+    {
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(@Nullable Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
 
+    // ======================================================================
+    // Methods
+    // ======================================================================
+
+    /*
+     * @see android.app.Activity#onCreate(android.os.Bundle)
+     */
+    @Override
+    protected void onCreate(
+        @Nullable
+        final Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+
+        setContentView( R.layout.activity_main );
+    }
+
+    /*
+     * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+     */
+    @Override
+    public boolean onCreateOptionsMenu(
+        @Nullable
+        final Menu menu )
+    {
+        getMenuInflater().inflate( R.menu.main, menu );
+        return true;
+    }
 }
