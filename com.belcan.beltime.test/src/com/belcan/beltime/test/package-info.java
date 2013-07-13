25530973b2abbfa5500869d0c36945dd3c218abd
Steven Soloff <s_soloff@yahoo.com>
2013-07-12 21:18:01 -0400

    Added missing package information file for the test package.

---
+++ com.belcan.beltime.test/src/com/belcan/beltime/test/package-info.java
@@ -0,0 +1,21 @@
+/*
+ * package-info.java
+ *
+ * Copyright 2013 Beltime contributors and others.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Public License v1.0, which
+ * accompanies this distribution and is available at
+ * <http://www.eclipse.org/legal/epl-v10.html>.
+ *
+ * Created on Jul 12, 2013 at 9:14:59 PM.
+ */
+
+/**
+ * Defines classes that support testing the Beltime application.
+ */
+@NonNullByDefault
+package com.belcan.beltime.test;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
