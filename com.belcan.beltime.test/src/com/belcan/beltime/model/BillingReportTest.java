d7c09791515e49e0566532919f0c3b9a1446fa47
Steven Soloff <s_soloff@yahoo.com>
2013-08-17 20:47:00 -0400

    Added the BillingReport class.

---
+++ com.belcan.beltime.test/src/com/belcan/beltime/model/BillingReportTest.java
@@ -0,0 +1,103 @@
+/*
+ * BillingReportTest.java
+ *
+ * Copyright 2013 Beltime contributors and others.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Public License v1.0, which
+ * accompanies this distribution and is available at
+ * <http://www.eclipse.org/legal/epl-v10.html>.
+ *
+ * Created on Aug 17, 2013 at 8:16:06 PM.
+ */
+
+package com.belcan.beltime.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import junit.framework.TestCase;
+
+/**
+ * A fixture for testing the {@link BillingReport} class.
+ */
+public final class BillingReportTest
+    extends TestCase
+{
+    // ======================================================================
+    // Fields
+    // ======================================================================
+
+    /** The billing report under test in the fixture. */
+    private BillingReport billingReport_;
+
+
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the {@code BillingReportTest} class.
+     */
+    public BillingReportTest()
+    {
+    }
+
+
+    // ======================================================================
+    // Methods
+    // ======================================================================
+
+    /*
+     * @see junit.framework.TestCase#setUp()
+     */
+    @Override
+    protected void setUp()
+        throws Exception
+    {
+        super.setUp();
+
+        billingReport_ = new BillingReport( new Date( 0L ), new Date( 3600000L ), new ArrayList<Bill>() );
+    }
+
+    /**
+     * Ensures the {@link BillingReport#getBeginDate} method returns a copy of
+     * the beginning date.
+     */
+    public void testGetBeginDate_ReturnsCopy()
+    {
+        final Date beginDate = billingReport_.getBeginDate();
+        final Date expectedBeginDate = new Date( beginDate.getTime() );
+        beginDate.setTime( beginDate.getTime() + 1L );
+
+        assertEquals( expectedBeginDate, billingReport_.getBeginDate() );
+    }
+
+    /**
+     * Ensures the {@link BillingReport#getBills} method returns a copy of the
+     * bills collection.
+     */
+    @SuppressWarnings( "null" )
+    public void testGetBills_ReturnsCopy()
+    {
+        final Collection<Bill> bills = billingReport_.getBills();
+        final Collection<Bill> expectedBills = new ArrayList<Bill>( bills );
+        bills.add( new Bill( TestChargeNumbers.CHARGE_NUMBER_1, 0L ) );
+
+        assertEquals( expectedBills, billingReport_.getBills() );
+    }
+
+    /**
+     * Ensures the {@link BillingReport#getEndDate} method returns a copy of the
+     * ending date.
+     */
+    public void testGetEndDate_ReturnsCopy()
+    {
+        final Date endDate = billingReport_.getEndDate();
+        final Date expectedEndDate = new Date( endDate.getTime() );
+        endDate.setTime( endDate.getTime() + 1L );
+
+        assertEquals( expectedEndDate, billingReport_.getEndDate() );
+    }
+}
---
+++ com.belcan.beltime/src/com/belcan/beltime/model/BillingReport.java
@@ -0,0 +1,111 @@
+/*
+ * BillingReport.java
+ *
+ * Copyright 2013 Beltime contributors and others.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Public License v1.0, which
+ * accompanies this distribution and is available at
+ * <http://www.eclipse.org/legal/epl-v10.html>.
+ *
+ * Created on Aug 17, 2013 at 8:06:15 PM.
+ */
+
+package com.belcan.beltime.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
+/**
+ * A billing report.
+ * 
+ * <p>
+ * A billing report consists of a collection of bills charged during a specified
+ * period of time.
+ * </p>
+ */
+public final class BillingReport
+{
+    // ======================================================================
+    // Fields
+    // ======================================================================
+
+    /** The beginning date of the report, inclusive. */
+    private final Date beginDate_;
+
+    /** The collection of bills charged during the time period of the report. */
+    private final Collection<Bill> bills_;
+
+    /** The ending date of the report, inclusive. */
+    private final Date endDate_;
+
+
+    // ======================================================================
+    // Constructors
+    // ======================================================================
+
+    /**
+     * Initializes a new instance of the {@code BillingReport} class.
+     * 
+     * @param beginDate
+     *        The beginning date of the report, inclusive; must not be
+     *        {@code null}. No copy is made of this value and it must not be
+     *        modified after calling this method.
+     * @param endDate
+     *        The ending date of the report, inclusive; must not be {@code null}
+     *        . No copy is made of this value and it must not be modified after
+     *        calling this method.
+     * @param bills
+     *        The collection of bills charged during the time period of the
+     *        report; must not be {@code null}. No copy is made of this
+     *        collection and it must not be modified after calling this method.
+     */
+    BillingReport(
+        final Date beginDate,
+        final Date endDate,
+        final Collection<Bill> bills )
+    {
+        beginDate_ = beginDate;
+        bills_ = bills;
+        endDate_ = endDate;
+    }
+
+
+    // ======================================================================
+    // Methods
+    // ======================================================================
+
+    /**
+     * Gets the beginning date of the report, inclusive.
+     * 
+     * @return The beginning date of the report, inclusive; never {@code null}.
+     */
+    public Date getBeginDate()
+    {
+        return new Date( beginDate_.getTime() );
+    }
+
+    /**
+     * Gets the collection of bills charged during the time period of the
+     * report.
+     * 
+     * @return The collection of bills charged during the time period of the
+     *         report; never {@code null}.
+     */
+    public Collection<Bill> getBills()
+    {
+        return new ArrayList<Bill>( bills_ );
+    }
+
+    /**
+     * Gets the ending date of the report, inclusive.
+     * 
+     * @return The ending date of the report, inclusive; never {@code null}.
+     */
+    public Date getEndDate()
+    {
+        return new Date( endDate_.getTime() );
+    }
+}
