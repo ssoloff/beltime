892fd01931335aae917030195e193c119743a11d
Steven Soloff <s_soloff@yahoo.com>
2013-08-23 23:04:17 -0400

    Added explicit sign to return value of Comparable.compareTo().

--- com.belcan.beltime/src/com/belcan/beltime/model/ChargeNumber.java
+++ com.belcan.beltime/src/com/belcan/beltime/model/ChargeNumber.java
@@ -65,7 +65,7 @@ public final class ChargeNumber
     {
         if( another == null )
         {
-            return 1;
+            return +1;
         }
 
         return value_.compareTo( another.value_ );
